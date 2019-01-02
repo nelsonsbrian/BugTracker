20460f00528c27a3681740127eec3799cd7648e0
Brian Nelson <nelsonsbrian@gmail.com>
2019-01-01 23:19:17 -0800

    add user model and create a new user

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const gatherings = require('../routes/gatherings');
+const users = require('../routes/users');
 
 module.exports = function(app) {
   app.use(express.json());
   app.use('/api/gatherings', gatherings);  
+  app.use('/api/users', users);  
 }

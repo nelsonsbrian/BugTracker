b049a19f1fd59bfc11cdaaf7a2352a148e227d19
Brian Nelson <nelsonsbrian@gmail.com>
2019-01-01 20:06:15 -0800

    add initial server code

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, console.log(`Listening on port ${port}...`));
+
+module.exports = server;

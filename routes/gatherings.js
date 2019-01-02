7c62727c35e5f4f0d5e2e8e37bd5ef0050fc50b5
Brian Nelson <nelsonsbrian@gmail.com>
2019-01-01 22:11:36 -0800

    add gathering pust by id endpoint

--- routes/gatherings.js
+++ routes/gatherings.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const {Gathering, validate} = require('../models/gathering');
+const { Gathering, validate } = require('../models/gathering');
 const router = express.Router();
 
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
   const gatherings = await Gathering.find().sort('name');
   res.send(gatherings);
 });
@@ -21,19 +21,37 @@ router.get('/:id', async (req, res) => {
 });
 
 
-router.post('/', async (req,res) => {
-  const {error} = validate(req.body);
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let gathering = new Gathering( {
+  let gathering = new Gathering({
     name: req.body.name,
     description: req.body.description,
-    dateCreated:  req.body.dateCreated,
-    dateHosted:  req.body.dateHosted,
+    dateCreated: req.body.dateCreated,
+    dateHosted: req.body.dateHosted,
   });
   gathering = await gathering.save();
-  
+
   res.send(gathering);
+});
+
+router.put('/:id', async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(404).send('Invalid ID to update.');
+  }
+
+  const gathering = await Gathering.findByIdAndUpdate(req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      dateHosted: req.body.dateHosted,
+    },
+    { new: true });
+
+    if (!gathering) return res.status(404).send('The gathering with the given ID was not found');
+
+    res.send(gathering);    
 });
 
 router.delete('/:id', async (req, res) => {
